3218ac3b7663bb9adf142ae4a27a98c8e2d8ee75
liluxian <694084812@qq.com>
2020-12-29 19:51:27 +0800

    test1

---
+++ Test_1/src/ComputeArea.java
@@ -0,0 +1,19 @@
+
+public class ComputeArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius;//Declare radius
+		double area;
+		
+		//Assign a radius
+		radius = 20;//radius is now 20
+		
+		//Compute area
+		area = radius*radius*3.14159;
+		
+		//Display results
+		System.out.println("The area for the ciecle of radius " + radius + " is "+area);
+	}
+
+}
