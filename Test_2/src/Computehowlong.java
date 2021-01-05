d126c97925e7ecdc04635e54600b905533521b1a
liluxian <694084812@qq.com>
2021-01-05 23:44:21 +0800

    Test2

---
+++ Test_2/src/Computehowlong.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Computehowlong {
+	public static void main(String[] args) {
+		final int radius=10;
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter a point two coordinates:");
+		double x=input.nextDouble();
+		double y=input.nextDouble();
+		double distance=Math.sqrt(x*x+y*y);
+		if(distance<=radius) {
+			System.out.println("Point ("+x+", "+y+") is in the circle");
+	}
+		else {
+			System.out.println("Point ("+x+", "+y+") is not in the circle");
+	}
+    }
+}
